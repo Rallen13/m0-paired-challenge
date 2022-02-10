1287cb482108471208b6288d8165485b6e6e1f13
Rachel Allen <rallenco13@gmail.com>
2022-02-09 21:21:11 -0700

    fixed mistakes

--- challenge/arbitrary_file.js
+++ challenge/arbitrary_file.js
@@ -1,16 +1,16 @@
 //I chose arbitrary file because there was no file name provided.
-var = email "Rallenco13@gmail.com";
+var libraryName = "Bailey Public Library";
 //declares a variable assigned to the string
-var = age 36;
-//declares a variable assigned to the number
-var = depositPaid = true;
+var libraryEmployees = 36;
+//declares a variable assigned to a number
+var checkedOut = true;
 //declares a variable assigned to a boolean
-var = name "Rachel";
+var librarianName = "Rachel";
 //declares the variable assigned to the string
-var = date "020922";
-//this is the date
-var = dogName "Kayuh";
-//this is my dogs name
+var numTotalBooks = "100000";
+//declares the variable assigned to a number
+var numCheckedOutBooks = "25000";
+//declares the variable assigned to a number
 
 var genres = ["Fiction", "Non-Fiction"];
 // assigned book genres to an array
