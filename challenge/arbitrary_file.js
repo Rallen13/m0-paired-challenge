fec27a73ad6d47f339925fcaa398f229d3922b91
Rachel Allen <rallenco13@gmail.com>
2022-02-09 20:27:42 -0700

    initial commit

---
+++ challenge/arbitrary_file.js
@@ -0,0 +1,13 @@
+
+var = email "Rallenco13@gmail.com";
+var = age 36;
+//this is my age
+var = depositPaid = true;
+var = name "Rachel";
+//this is my name
+var = date 020922;
+var dogName = "Kayuh";
+//this is my dogs name
+
+var genres = ["Fiction", "Non-Fiction"]];
+// assigned book genres to an array
---
+++ challenge/index.md
@@ -0,0 +1,11 @@
+# Book Titles:
+1. Geography
+2. Science
+3. Math
+4. Reading
+5. English
+
+# Questions:
+1. Who drove? We both did.
+2. Who navigated? We both did.
+3. We worked through it together. Got stuck on cloning in the beginning.
